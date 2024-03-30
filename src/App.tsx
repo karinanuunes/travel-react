efb2cf0593ecc5c4c34947d17c284ecd33b1083c
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-03-29 22:39:53 -0300

    add logo, subscribe, footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Category from "./components/category/category";
 import Destination from "./components/destination/destination";
 import Book from "./components/book/book";
 import Assessment from "./components/assessment/assessment";
+import Logos from "./components/logos/logos";
+import Subscribe from "./components/subscribe/subscribe";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
       <Destination />
       <Book />
       <Assessment />
+      <Logos />
+      <Subscribe />
+      <Footer />
     </>
   );
 }
