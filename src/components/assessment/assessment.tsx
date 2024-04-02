b3c875bf3d2187336b8179772a85949970f7e520
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-04-01 21:19:46 -0300

    ajust props error

--- src/components/assessment/assessment.tsx
+++ src/components/assessment/assessment.tsx
@@ -12,16 +12,14 @@ const Assessment = () => {
         <button className="arrow-button">
           <svg
             width="50px"
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            stroke-linejoin="round"
-            stroke-miterlimit="2"
+            fillRule="evenodd"
+            strokeLinejoin="round"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
               d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591zm-.289 7.563v-5.446l-3.522 2.719z"
-              fill-rule="nonzero"
+              fillRule="nonzero"
             />
           </svg>
         </button>
@@ -49,16 +47,14 @@ const Assessment = () => {
         <button className="arrow-button">
           <svg
             width="50px"
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            stroke-linejoin="round"
-            stroke-miterlimit="2"
+            fillRule="evenodd"
+            strokeLinejoin="round"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
               d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591zm.289 7.563v-5.446l3.522 2.719z"
-              fill-rule="nonzero"
+              fillRule="nonzero"
             />
           </svg>
         </button>
